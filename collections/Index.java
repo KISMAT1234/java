135778c90a595fdd7b034ca7393bcea9e5f638f3
KISMAT1234 <kismatbohora123@gmail.com>
2025-04-22 15:12:23 +0545

    enhanced loop and foreach loop  checked

--- collections/Index.java
+++ collections/Index.java
@@ -1,11 +1,17 @@
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Index {
-    public static void main(String[] args) {
+// public class Index {
+//     public static void main(String[] args) {
         // String nums[] = {"nepal", "ocean"};
         // int types[] = new int[5];
         // System.out.println(types.length);
@@ -21,25 +27,88 @@ public class Index {
         // nums.add(9);
         // nums.add(1); //supports duplicate value
 
-        Set<Integer> nums = new HashSet<Integer>();
-        nums.add(1);
-        nums.add(4);
-        nums.add(9);
-        nums.add(5);
-        nums.add(2);
-        nums.add(1);
+        // Set<Integer> nums = new HashSet<Integer>();
+        // nums.add(1);
+        // nums.add(4);
+        // nums.add(9);
+        // nums.add(5);
+        // nums.add(2);
+        // nums.add(1);
 
+        // System.out.println(nums);
+        // System.out.println(nums.size());
+        // System.out.println(nums.getClass());
+        // System.out.println(nums.isEmpty());
+
+        // for( int data : nums){
+        //     System.out.println(data+ "value");
+        //     // System.out.println(data);
+        // }
+
+        
+        // Set<Integer> nums = new TreeSet<Integer>();
+        // nums.add(1);
+        // nums.add(6);
+        // nums.add(3);
+        // nums.add(9);
+        // nums.add(4);
+
+        // System.out.println(nums + "value");
+
+        // Map students = new HashMap();
+        // students.put("math", 20);
+        // students.put("english", 50);
+        // students.put("nepali", 70);
+        // students.put("social", 80);
+        // students.put("social", 90);
+
+        // System.out.println(students + "students");
+        // System.out.println(students.get("social"))
+
+//     }
+// }
+
+//Comparator
+// public class Index {
+//     public static void main(String[] args) {
+//         Comparator<Integer> com = new Comparator<Integer>() {
+//             public int compare(Integer i , Integer j){
+//                 if(i%10 > j%10){
+//                     return 1;
+//                 }
+//                 return -1;
+//             }
+//         };
+//        List<Integer> nums = new ArrayList<>();
+//        nums.add(1);
+//        nums.add(4);
+//        nums.add(3);
+//        nums.add(2);
+//        nums.add(9);
+//        System.out.println(nums + "before");
+
+//        Collections.sort(nums, com);
+
+//        System.out.println(nums);
+//     }
+// }
+
+
+
+public class Index {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,5,7);
         System.out.println(nums);
-        System.out.println(nums.size());
-        System.out.println(nums.getClass());
-        System.out.println(nums.isEmpty());
-        // System.out.println(nums.get(2));
-        // System.out.println(nums.indexOf(9));
 
-        for( int data : nums){
-            System.out.println(data+ "value");
-            // System.out.println(data);
+        int sum = 0;
+        for(int n : nums){
+            if(n%2 == 0){
+                n = n*2;
+                sum += n;
+            }
         }
+        System.out.println(sum);
 
+        nums.forEach(items -> System.out.println(items));
     }
 }
